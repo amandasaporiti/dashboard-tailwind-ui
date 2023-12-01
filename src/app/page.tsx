8fb8e14ce83a31f8ed7914e6221caca4f8d2ef54
amandasaporiti <amandasaporiti@gmail.com>
2023-12-01 16:00:42 -0300

    feat:  Eslint and prettier configs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div>
-      <h1 className="text-amanda text-6xl justify-center font-black">
+      <h1 className="text-amanda justify-center text-6xl font-black">
         Hello Tailwind
       </h1>
     </div>
