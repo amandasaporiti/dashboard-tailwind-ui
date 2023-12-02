bbcbbb0c02496bba45bf7fed7503f853d8ec8385
amandasaporiti <amandasaporiti@gmail.com>
2023-12-02 01:29:44 -0300

    feat: Create Input Component

---
+++ src/components/Input.tsx
@@ -0,0 +1,29 @@
+import { ComponentProps, InputHTMLAttributes } from 'react'
+
+type InputPrefix = ComponentProps<'div'>
+
+export function InputPrefix(props: InputPrefix) {
+  return <div {...props} />
+}
+
+type InputControlProps = InputHTMLAttributes<HTMLInputElement>
+
+export function InputControl(rest: InputControlProps) {
+  return (
+    <input
+      className="flex-1 border-0 bg-transparent p-0 text-zinc-900 placeholder-zinc-600"
+      {...rest}
+    />
+  )
+}
+
+type InputRootProps = ComponentProps<'div'>
+
+export function InputRoot(props: InputRootProps) {
+  return (
+    <div
+      className="mx-1 flex w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm"
+      {...props}
+    />
+  )
+}
