78a43f5f8cf462d69cee91324f2b38019bed3d78
amandasaporiti <amandasaporiti@gmail.com>
2023-12-13 15:38:09 -0300

    feat: create Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,209 @@
+import { InputControl, InputPrefix, InputRoot } from '@/components/Input'
+import { Bold, Italic, Link, List, ListOrdered, Mail } from 'lucide-react'
+import * as FileInput from '@/components/Form/components/FileInput'
+import { Select } from '@/components/Form/components'
+import { SelectItem } from '@/components/Form/components/Select/SelectItem'
+import { Textarea } from '@/components/Form/components/Textarea'
+import { Button } from '@/components/Button'
+
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-amanda justify-center text-6xl font-black">
-        Hello Tailwind
-      </h1>
-    </div>
+    <>
+      <h1 className="text-3xl font-medium text-zinc-900">Settings</h1>
+      <div className="mt-6 flex flex-col">
+        <header className="flex items-center justify-between border-b border-zinc-200 pb-5">
+          <div className="space-y-1">
+            <h2 className="text-lg font-medium text-zinc-900">Personal info</h2>
+            <span className="text-sm text-zinc-500">
+              Update your photo and personal details here.
+            </span>
+          </div>
+          <div className="flex items-center gap-2">
+            <Button type="button" variant="outline">
+              Cancel
+            </Button>
+            <Button variant="primary" type="submit" form="settings">
+              Save
+            </Button>
+          </div>
+        </header>
+        <form
+          id="settings"
+          className="mt-6 flex w-full flex-col gap-5 divide-y divide-zinc-200"
+        >
+          <div className="grid grid-cols-form gap-3">
+            <label
+              htmlFor="firstName"
+              className="text-sm font-medium text-zinc-700"
+            >
+              Name
+            </label>
+            <div className="grid grid-cols-2 gap-6">
+              <InputRoot>
+                <InputControl defaultValue="Amanda" id="firstName" />
+              </InputRoot>
+
+              <InputRoot>
+                <InputControl defaultValue="Saporiti" />
+              </InputRoot>
+            </div>
+          </div>
+
+          <div className="grid grid-cols-form gap-3 pt-5">
+            <label
+              htmlFor="emailAddress"
+              className="text-sm font-medium text-zinc-700"
+            >
+              E-mail Address
+            </label>
+
+            <InputRoot>
+              <InputPrefix>
+                <Mail className="h-5 w-5 text-zinc-500" />
+              </InputPrefix>
+              <InputControl
+                defaultValue="amandasaporiti@email.com"
+                id="emailAddress"
+                type="email"
+              />
+            </InputRoot>
+          </div>
+
+          <div className="grid grid-cols-form gap-3 pt-5">
+            <label
+              htmlFor="photo"
+              className="flex flex-col gap-0.5 text-sm font-medium text-zinc-700"
+            >
+              Your photo
+              <span className="text-sm font-normal text-zinc-500">
+                This will be displayed on your profile.
+              </span>
+            </label>
+
+            <FileInput.Root className="flex items-start gap-5">
+              <FileInput.ImagePreview />
+              <FileInput.Trigger />
+              <FileInput.Control />
+            </FileInput.Root>
+          </div>
+
+          <div className="grid grid-cols-form gap-3 pt-5">
+            <label htmlFor="role" className="text-sm font-medium text-zinc-700">
+              Role
+            </label>
+
+            <InputRoot>
+              <InputControl defaultValue="CTO" id="role" />
+            </InputRoot>
+          </div>
+
+          <div className="grid grid-cols-form gap-3 pt-5">
+            <label
+              htmlFor="country"
+              className="text-sm font-medium text-zinc-700"
+            >
+              Country
+            </label>
+            <Select placeholder="Select a country..">
+              <SelectItem value="br" text="Brazil" />
+              <SelectItem value="us" text="United States" />
+              <SelectItem value="fr" text="França" />
+            </Select>
+          </div>
+
+          <div className="grid grid-cols-form gap-3 pt-5">
+            <label
+              htmlFor="timezone"
+              className="text-sm font-medium text-zinc-700"
+            >
+              Timezone
+            </label>
+
+            <Select placeholder="Select a timezone..">
+              <SelectItem
+                value="fnt"
+                text="Fernando de Noronha (FNT) UTC-02:00"
+              />
+              <SelectItem value="brt" text="Brasília (BRT) UTC-03:00" />
+              <SelectItem value="amt" text="Amazon Time (AMT) UTC-05:00" />
+            </Select>
+          </div>
+
+          <div className="grid grid-cols-form gap-3 pt-5">
+            <label
+              htmlFor="bio"
+              className="flex flex-col gap-0.5 text-sm font-medium text-zinc-700"
+            >
+              Bio
+              <span className="text-sm font-normal text-zinc-500">
+                Write a short introduction.
+              </span>
+            </label>
+            <div className="space-y-3">
+              <div className="grid grid-cols-2 gap-3">
+                <Select placeholder="" defaultValue="normal-text">
+                  <SelectItem
+                    value="normal-text"
+                    defaultChecked
+                    text="Normal text"
+                  />
+                  <SelectItem value="markdown" text="Markdown" />
+                </Select>
+                <div className="flex items-center gap-1">
+                  <Button variant="ghost" type="button">
+                    <Bold className="h-4 w-4 text-zinc-500" strokeWidth={3} />
+                  </Button>
+                  <Button variant="ghost" type="button">
+                    <Italic className="h-4 w-4 text-zinc-500" strokeWidth={3} />
+                  </Button>
+                  <Button variant="ghost" type="button">
+                    <Link className="h-4 w-4 text-zinc-500" strokeWidth={3} />
+                  </Button>
+                  <Button variant="ghost" type="button">
+                    <List className="h-4 w-4 text-zinc-500" strokeWidth={3} />
+                  </Button>
+                  <Button variant="ghost" type="button">
+                    <ListOrdered
+                      className="h-4 w-4 text-zinc-500"
+                      strokeWidth={3}
+                    />
+                  </Button>
+                </div>
+              </div>
+              <Textarea
+                id="bio"
+                defaultValue="I'm a Product Designer based in Melbourne, Australia. I specialize in UX/UI design, brand strategy, and Webflow development."
+              />
+            </div>
+          </div>
+
+          <div className="grid grid-cols-form gap-3 pt-5">
+            <label
+              htmlFor="portfolioProjects"
+              className="flex flex-col gap-0.5 text-sm font-medium text-zinc-700"
+            >
+              Portfolio Projects
+              <span className="text-sm font-normal text-zinc-500">
+                Share a few snippets of your work.
+              </span>
+            </label>
+            <FileInput.Root>
+              <FileInput.Trigger />
+              <FileInput.Control multiple />
+              <FileInput.FileList />
+            </FileInput.Root>
+          </div>
+
+          <footer className="flex items-center justify-end gap-3 pt-8">
+            <Button type="button" variant="outline">
+              Cancel
+            </Button>
+            <Button variant="primary" type="submit">
+              Save
+            </Button>
+          </footer>
+        </form>
+      </div>
+    </>
   )
 }
